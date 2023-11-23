15bfe93bdb295023d80238b87c2506196e8dbb24
Tomas Vargas <tavargas9@gmail.com>
2023-11-22 22:31:27 -0700

    add update tag name put routing

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -48,8 +48,22 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updatedTag = await Tag.update(
+    {
+			tag_name: req.body.tag_name,
+		},
+    {
+			where: {
+				id: req.params.id,
+			},
+		});
+    res.json(updatedTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
