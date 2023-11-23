cd5f6cbd7d063377df71e9254704460cbc376dca
Tomas Vargas <tavargas9@gmail.com>
2023-11-22 20:50:58 -0700

    define ProductTag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,25 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      references: {
+        model: 'product',
+        key: 'id'
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      },
+    }
   },
   {
     sequelize,
